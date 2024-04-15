957c47b986ce6436eae2029db79e0fb3dd8386ae
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-04-14 23:57:56 -0500

    Refactor file paths and imports in the code blocks, and provide ui and auth features in the app root

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,31 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { uiFeature } from './shared/state/ui.state';
+import { authFeature } from './auth/store/auth.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore(),
+    provideState(uiFeature),
+    provideState(authFeature),
     importProvidersFrom(
       provideFirebaseApp(() => initializeApp(environment.firebase))
     ),
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideFirestore(() => getFirestore())),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };

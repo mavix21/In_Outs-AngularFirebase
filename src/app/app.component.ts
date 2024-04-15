444262411a8c5a54c47a8a9bc8d200c67eba3c51
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2024-04-14 23:56:55 -0500

    Listen to auth status changes and set/unset the user accordingly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthService } from './services/auth.service';
+import * as firebase from '@angular/fire/auth';
+import { Store } from '@ngrx/store';
+
+import { AuthService } from './auth/services/auth.service';
+import { AppState } from './app.state';
+import { setUser, unsetUser } from './auth/store/auth.actions';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +16,18 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent {
   private authService = inject(AuthService);
+  private store: Store<AppState> = inject(Store);
+  private auth: firebase.Auth = inject(firebase.Auth);
 
   constructor() {
     // Listen to auth status changes
-    this.authService.firebaseAuthListener().subscribe();
+    this.authService.firebaseAuthListener().subscribe((user) => {
+      if (user) {
+        console.log('Setting user ... : ', user);
+        this.store.dispatch(setUser({ user }));
+      } else {
+        this.store.dispatch(unsetUser());
+      }
+    });
   }
 }
